49c63023702b4e1e659bc127622cacb1a7abf2c5
Aydin Pramasta <pramastaaydin@gmail.com>
2022-10-28 16:50:44 +0700

    remove class field from User

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,6 @@ class User extends Authenticatable
         'name',
         'username',
         'role',
-        'class',
         'phone',
         'password',
     ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('username')->unique();
             $table->string('role');
-            $table->string('class')->nullable();
             $table->string('phone');
             $table->string('password');
             $table->rememberToken();
